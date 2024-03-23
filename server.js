30f4987f0cb1b56062c3a4948cc7590db4d5827b
ohakari <150848708+ohakari@users.noreply.github.com>
2024-03-23 17:58:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Change "*" if needed
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5000'); // Change "*" if needed
     res.setHeader('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
     next();
